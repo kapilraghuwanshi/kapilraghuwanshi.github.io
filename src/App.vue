c66820b605c580989c08df25e41143913a1944f3
Kapil Raghuwanshi <kraghuwanshi@conversantmedia.com>
2020-10-17 14:30:06 +0530

    Initial Commit - Started with portfolio website

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="kapilraghuwanshi logo" src="./assets/kapilraghuwanshi_logo.png" />
+  <Home msg="Hello Kapil" />
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>Edit <code>components/Home.vue</code> to test hot module replacement.</p>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
